b708fafae7ba0678bf73a82f4eb76766af7eccb1
ssfhc <ssfhc594@gmail.com>
2023-03-07 09:55:53 +0900

    20230307

--- src/main/java/com/tech/kimchi/controller/UserInfoController.java
+++ src/main/java/com/tech/kimchi/controller/UserInfoController.java
@@ -1,6 +1,7 @@
 package com.tech.kimchi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tech.kimchi.service.KimchiServiceInter;
 
@@ -9,6 +10,9 @@ public class UserInfoController {
 
 	KimchiServiceInter kimchiServiceInter;
 	
-	
-	
+	@RequestMapping("/UserInfo/loginView")
+	public String loginView() {
+		System.out.println("UserInfoController/loginView");
+		return "/UserInfo/loginView";
+	}
 }
